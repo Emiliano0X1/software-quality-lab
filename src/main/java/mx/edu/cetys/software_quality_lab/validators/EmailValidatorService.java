62e0c593c0e99e9423ca5b14a6ab3c52d4031996
Emiliano0X1 <egp2506@gmail.com>
2026-02-26 13:06:31 -0800

    Activity of EmailValidator Complete with tests and logic code

--- src/main/java/mx/edu/cetys/software_quality_lab/validators/EmailValidatorService.java
+++ src/main/java/mx/edu/cetys/software_quality_lab/validators/EmailValidatorService.java
@@ -1,12 +1,138 @@
 package mx.edu.cetys.software_quality_lab.validators;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class EmailValidatorService {
+
+    private Set<Character> validChars = new HashSet<>(Arrays.asList('_','.','+','-','#'));
+    private Set<Character> validUserChars = new HashSet<>(Arrays.asList('_','-','.','+'));
 
     public boolean isValid(String email) {
         if(email == null || email.isEmpty()){
             return false;
         }
 
+        //1. Characters valid (1-0 and a-z)
+        if(!hasCharactersValid(email)){
+            return false;
+        };
+
+        //3. Check if the string has # and if yes divide and if not return false
+        String[] emailParts = dividedStringsByHashTag(email);
+        if(emailParts.length == 0){
+            return false;
+        }
+
+        String user = emailParts[0];
+        String provider_domain = emailParts[1];
+
+        if(user.isEmpty() || provider_domain.isEmpty()){
+            return false;
+        }
+
+        //2 Check if the user has valid characters
+        if(!userEmailHasValidChars(user)){
+            return false;
+        }
+
+        //2.1 Check if the provider+domain has validCahracters
+        if(!provider_domainHasValidChars(provider_domain)){
+            return false;
+        }
+
+        //4. No hay diptongo
+        if(userHasDiptongo(user)){
+            return false;
+        }
+
+        //5. Check domain size
+        String[] provider_domain_parts = provider_domain.split("\\.");
+        String provider = provider_domain_parts[0];
+        String domain = provider_domain_parts[1];
+
+        if(domain.length() < 1 || domain.length() > 5){
+            return false;
+        }
+
+        //6. Check the size of the email
+        if(email.length() > 47){
+            return false;
+        }
+
+        //7. Check if the email has 4
+        if(!email.contains("4")){
+            return false;
+        }
+
         return true;
     }
+
+    private boolean hasCharactersValid(String email) {
+        for(char c : email.toCharArray()){
+
+            if(Character.isUpperCase(c)){
+                return false;
+            }
+
+            if(!Character.isLetterOrDigit(c)) {
+                if(!validChars.contains(c)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean userEmailHasValidChars(String user) {
+        for(char c : user.toCharArray()){
+            if(!Character.isLetterOrDigit(c)){
+                if(!validUserChars.contains(c)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private boolean provider_domainHasValidChars(String provider_domain) {
+        for(char c : provider_domain.toCharArray()) {
+            if(!Character.isLetterOrDigit(c)){
+                if(c != '.'){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private String[] dividedStringsByHashTag(String email) {
+
+        if(email.contains("#")){
+            String[] splitedEmail = email.split("#");
+            if(splitedEmail.length <= 1){
+                return new String[0];
+            }
+
+            return splitedEmail;
+        }
+
+        return new String[0];
+    }
+
+    private boolean userHasDiptongo(String user) {
+        if(user.length() < 2){
+            return true;
+        }
+
+        if(user.charAt(0) == user.charAt(1)){
+            return true;
+        }
+
+        return false;
+    }
+
+
 }
--- src/test/java/mx/edu/cetys/software_quality_lab/validators/EmailValidatorServiceTest.java
+++ src/test/java/mx/edu/cetys/software_quality_lab/validators/EmailValidatorServiceTest.java
@@ -3,6 +3,7 @@ package mx.edu.cetys.software_quality_lab.validators;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class EmailValidatorServiceTest {
 
@@ -29,4 +30,69 @@ public class EmailValidatorServiceTest {
         //Assert
         assertFalse(isValid);
     }
+
+    @Test
+    void shouldReturnFalse_WhenEmailHasInvalidCharacters(){
+        EmailValidatorService emailValidate = new EmailValidatorService();
+        var isValid = emailValidate.isValid("#$%lo4#gmail.com");
+        assertFalse(isValid);
+    }
+
+    @Test
+    void shouldReturnFalse_WhenEmailUserHasInvalidCharacters(){
+        EmailValidatorService emailValidate = new EmailValidatorService();
+        var isValid = emailValidate.isValid("emiAliano4#gmail.com");
+        assertFalse(isValid);
+    }
+
+    @Test
+    void shouldReturnFalse_WhenEmailProvideAndDomainHasInvalidCharacters(){
+        EmailValidatorService emailValidate = new EmailValidatorService();
+        var isValid = emailValidate.isValid("emiliano4#gmail/.com");
+        assertFalse(isValid);
+    }
+
+    @Test
+    void shouldReturnFalse_WhenEmailDivisorIsNotHashtag(){
+        EmailValidatorService emailValidate = new EmailValidatorService();
+        var isValid = emailValidate.isValid("emiliano4@gmail.com");
+        assertFalse(isValid);
+    }
+
+    @Test
+    void shouldReturnFalse_WhenEmailStartsWithDiptongo(){
+        EmailValidatorService emailValidate = new EmailValidatorService();
+        var isValid = emailValidate.isValid("eemiliano4#gmail.com");
+        assertFalse(isValid);
+    }
+
+    @Test
+    void shouldReturnFalse_WhenEmailDomainIsNotProperLength(){
+        EmailValidatorService emailValidate = new EmailValidatorService();
+        var isValid = emailValidate.isValid("emiliano4#gmail.commer");
+        assertFalse(isValid);
+    }
+
+    @Test
+    void shouldReturnFalse_WhenEmailLengthIsBiggerThan47(){
+        EmailValidatorService emailValidate = new EmailValidatorService();
+        var isValid = emailValidate.isValid("emilianojdsibfusndjfsdfojsdnfsdofjsifjsdifjsohdfosdhifiohsdfo4#gmail.com");
+        assertFalse(isValid);
+    }
+
+    @Test
+    void shouldReturnFalse_WhenEmailDoesNotContain4(){
+        EmailValidatorService emailValidate = new EmailValidatorService();
+        var isValid = emailValidate.isValid("emiliano#gmail.com");
+        assertFalse(isValid);
+    }
+
+    @Test
+    void shouldReturnTrue_WhenEmailIsValid(){
+        EmailValidatorService emailValidate = new EmailValidatorService();
+        var isValid = emailValidate.isValid("emil-_iano4#gmail.com");
+        assertTrue(isValid);
+    }
+
+
 }
